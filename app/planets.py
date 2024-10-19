4fa08959bc4ebc3e933209a82d4c8a5f5c5860c3
Brianna Root <brianna.k.root@gmail.com>
2024-10-18 22:37:08 -0600

    Added commas after each element in the planets list.

--- app/planets.py
+++ app/planets.py
@@ -6,13 +6,13 @@ class Planet:
         self.num_moons = num_moons
 
 planets = [
-    Planet(1, "Mercury", "the closest to the Sun", 0)
-    Planet(2, "Venus", "similar in size to Earth", 0)
-    Planet(3, "Earth", "suitable for live", 1)
-    Planet(4, "Mars",  "red", 2)
-    Planet(5, "Jupiter", "the largest planet", 95)
-    Planet(6, "Saturn","has rings", 146)
-    Planet(7, "Uranus", "blue gas giant", 27)
+    Planet(1, "Mercury", "the closest to the Sun", 0),
+    Planet(2, "Venus", "similar in size to Earth", 0),
+    Planet(3, "Earth", "suitable for live", 1),
+    Planet(4, "Mars",  "red", 2),
+    Planet(5, "Jupiter", "the largest planet", 95),
+    Planet(6, "Saturn","has rings", 146),
+    Planet(7, "Uranus", "blue gas giant", 27),
     Planet(8, "Neptune", "dark and cold", 16)
 
 ]        
