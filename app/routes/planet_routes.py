e35ea0935e485a0994e665c09aa0368ca01184de
Brianna Root <brianna.k.root@gmail.com>
2024-10-20 13:59:45 -0500

    Created the blueprint for planets_bp, created the decorator, and used get medthod and created function to create a list of all the planets.

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,3 +1,18 @@
 from flask import Blueprint
+from ..models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@planets_bp.get("")
+def get_all_planets():
+    results_list = []
+
+    for planet in planets:
+        results_list.append(dict(
+            id=planet.id, 
+            name=planet.name,
+            description=planet.description,
+            num_moond=planet.num_moons
+        ))
+
+    return results_list
