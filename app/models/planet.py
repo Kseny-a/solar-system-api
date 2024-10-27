fa2bc16935e51c539606b4e596aa9e4f02d8de2c
Brianna Root <brianna.k.root@gmail.com>
2024-10-28 10:49:03 +1300

    Setup planet model and created a migration. Added table for planets.

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,27 +1,36 @@
-class Planet:
-    def __init__(self, id, name, description, num_moons):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.num_moons = num_moons
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    num_moons: Mapped[int]
+
+# class Planet:
+#     def __init__(self, id, name, description, num_moons):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.num_moons = num_moons
     
-    def to_dict(self):
-        return dict(
-            id = self.id,
-            name = self.name,
-            description = self.description,
-            num_moons = self.num_moons,
-            )     
+#     def to_dict(self):
+#         return dict(
+#             id = self.id,
+#             name = self.name,
+#             description = self.description,
+#             num_moons = self.num_moons,
+#             )     
 
-planets = [
-    Planet(1, "Mercury", "the closest to the Sun", 0),
-    Planet(2, "Venus", "similar in size to Earth", 0),
-    Planet(3, "Earth", "suitable for live", 1),
-    Planet(4, "Mars",  "red", 2),
-    Planet(5, "Jupiter", "the largest planet", 95),
-    Planet(6, "Saturn","has rings", 146),
-    Planet(7, "Uranus", "blue gas giant", 27),
-    Planet(8, "Neptune", "dark and cold", 16)
+# planets = [
+#     Planet(1, "Mercury", "the closest to the Sun", 0),
+#     Planet(2, "Venus", "similar in size to Earth", 0),
+#     Planet(3, "Earth", "suitable for live", 1),
+#     Planet(4, "Mars",  "red", 2),
+#     Planet(5, "Jupiter", "the largest planet", 95),
+#     Planet(6, "Saturn","has rings", 146),
+#     Planet(7, "Uranus", "blue gas giant", 27),
+#     Planet(8, "Neptune", "dark and cold", 16)
 
-]        
+# ]        
 
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,34 +1,34 @@
 from flask import Blueprint, abort, make_response
-from ..models.planet import planets
+from app.models.planet import Planet
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-@planets_bp.get("")
-def get_all_planets():
-    results_list = []
+# @planets_bp.get("")
+# def get_all_planets():
+#     results_list = []
 
-    for planet in planets:
-        results_list.append(dict(
-            id=planet.id, 
-            name=planet.name,
-            description=planet.description,
-            num_moon=planet.num_moons
-        ))
+#     for planet in planets:
+#         results_list.append(dict(
+#             id=planet.id, 
+#             name=planet.name,
+#             description=planet.description,
+#             num_moon=planet.num_moons
+#         ))
 
-    return results_list
+#     return results_list
 
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
-    return planet.to_dict(), 200
+# @planets_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
+#     return planet.to_dict(), 200
 
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"message": f"Planet {planet_id} is invalid."}, 400))
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
-    abort(make_response({"message": f"Planet {planet_id} is not found."}, 404))    
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(make_response({"message": f"Planet {planet_id} is invalid."}, 400))
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
+#     abort(make_response({"message": f"Planet {planet_id} is not found."}, 404))    
